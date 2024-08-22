2260847f06bd9c73e3cf17912d482770da226f57
Hax <hax.multitool@gmail.com>
2024-08-22 22:25:35 +0200

    Add files via upload

---
+++ Loading2.0.bat
@@ -0,0 +1,193 @@
+@echo off
+@title Loading
+@mode 36, 16
+:x
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^|
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^/
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^-
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^\
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^|
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^/
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^-
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+if exist "signal.txt" (
+    del "signal.txt"
+    exit
+)
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo             Generating...
+echo.
+echo                  ^\
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+ping localhost -n 1 -w 50 > nul
+goto X
